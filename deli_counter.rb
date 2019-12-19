f427a88048930c709bff4cc6d6ad35554c7216c1
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-19 05:05:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+require 'pry'
+katz_deli=[]
+def line(katz_deli)
+  if katz_deli.length < 1
+    puts "The line is currently empty."
+
+  elsif katz_deli.length >=1
+     message = "The line is currently:"
+#     binding.pry
+katz_deli.each_with_index {|name, index| message << " #{index+1}. #{name}"}
+puts message
+  end
+
+end
+
+def take_a_number(katz_deli,"")
+takenum = "Welcome, #{name}."
+names.each_with_index {|name, index| takenum << "You are number #{index} in line."}
+puts takenum
+end
